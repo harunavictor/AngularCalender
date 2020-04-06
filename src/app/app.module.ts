802a4c765ae20882c444090494e3ff420714fea8
Haruna Victor <harunavictor754@gmail.com>
2020-04-06 17:04:41 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { DailySelectionComponent } from './daily-selection/daily-selection.component';
+import { WeeklySelectionComponent } from './weekly-selection/weekly-selection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DailySelectionComponent,
+    WeeklySelectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
